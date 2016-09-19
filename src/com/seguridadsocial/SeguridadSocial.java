43b081aabdc4f9a9666fbd75dae05628ee42d00e
Ferran Torredeflot <ftorredeflot@gmail.com>
2016-09-19 20:28:31 +0200

    classe persona creada

--- src/com/seguridadsocial/SeguridadSocial.java
+++ src/com/seguridadsocial/SeguridadSocial.java
@@ -1,7 +1,31 @@
 package com.seguridadsocial;
+import java.util.*;
 
-/**
- * Created by ferran on 19/09/16.
- */
-public class SeguridadSocial {
+public class SeguridadSocial {    private List<Persona> personasList;
+    public SeguridadSocial() {
+        personasList = new ArrayList<>();
+    }
+    // Debes comprobar que no se introduzcan dos personas con el mismo DNI/Número Seguridad Social
+    public void altaPersona(Persona persona) {
+
+    }
+    public void bajaPersona(String dni) {
+    }
+    public Persona obtenerPersonaPorDNI(String dni) {
+    }
+    public Persona obtenerPersonaPorNumSS(String numSS) {
+    }
+    public List<Persona> obtenerPersonasRangoSalarial(double min, double max){
+    }
+    public List<Persona> obtenerPersonasMayoresQue(int edad){
+    }
+    public List<Persona> obtenerTodas(){
+    }
+    @Override
+    public String toString() {
+        return "SeguridadSocial{" +
+                "personasList=" + personasList +
+                '}';
+    }
+
 }
